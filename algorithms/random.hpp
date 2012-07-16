47c64de70ae6ba655c33aa931fa12a613b40ecbe
BreakDS <breakds@gmail.com>
2012-07-15 19:10:15 -0500

    add random.hpp

---
+++ algorithms/random.hpp
@@ -0,0 +1,20 @@
+/*********************************************************************************
+ * File: random.hpp
+ * Description: random number generating algorithms
+ * by BreakDS, @ University of Wisconsin-Madison, Sun Jul 15 15:56:21 CDT 2012
+ *********************************************************************************/
+
+#pragma once
+#include <vector>
+
+namespace rangen
+{
+
+  // Sample k elements from 0..n-1, with no replace 
+  inline vector<int> randperm( const int n, const int k )
+  {
+    
+  }
+
+};
+
