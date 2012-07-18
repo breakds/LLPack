ba70604ba5aa98b38c2b47d9f73ed22c690629da
BreakDS <breakds@gmail.com>
2012-07-18 14:42:42 -0500

    pathname.hpp bug fixed

--- utils/pathname.hpp
+++ utils/pathname.hpp
@@ -4,6 +4,9 @@
  * by BreakDS, @ University of Wisconsin-Madison, Fri Jul 13 23:55:03 CDT 2012
  *********************************************************************************/
 
+#pragma once
+
+#include <vector>
 #include <string>
 #include "LLPack/utils/extio.hpp"
 
@@ -12,12 +15,12 @@ namespace path
 
   // build Triplet file list (FFFL) = Folder + Filename + postFix List
   template<typename listType>
-  vector<string> FFFL( const string& folder, const listType& filelist, const string postfix="" )
+  std::vector<std::string> FFFL( const std::string& folder, const listType& filelist, const std::string postfix="" )
   {
-    vector<string> tmp;
+    std::vector<std::string> tmp;
     tmp.resize( filelist.size() );
     for ( int i=0; i<filelist.size(); i++ ) {
-      tmp[i] = folder + "/" + static_cast<string>( filelist[i] ) + postfix;
+      tmp[i] = folder + "/" + static_cast<std::string>( filelist[i] ) + postfix;
     }
     return tmp;
   }
